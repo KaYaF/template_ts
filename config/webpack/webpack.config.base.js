fcaebe0d60ecebf75bf80735b480754511ac4747
KaYaF <d.guitar.c@gmail.com>
2021-11-05 23:54:09 +0900

    build: introduce webpack

---
+++ config/webpack/webpack.config.base.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        use: ['json-loader'],
+      },
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['node_modules'],
+  },
+  output: {
+    publicPath: './../../dist/',
+  },
+  optimization: {
+    chunkIds: 'named',
+  },
+};
